06984a67d96dc74ef7f9da0f48e5b335b00af258
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-06-24 01:05:26 -0500

    feat: Create Interfaces

---
+++ src/app/interfaces/education.interface.ts
@@ -0,0 +1,7 @@
+export interface Education{
+  institution: string,
+  degree: string,
+  period: string,
+  extra: string,
+  image: string
+}
---
+++ src/app/interfaces/reference.interface.ts
@@ -0,0 +1,7 @@
+export interface Reference {
+  name: string,
+  company: string,
+  position: string,
+  phone: string,
+  email: string
+}
