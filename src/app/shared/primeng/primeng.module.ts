85d99df47a716c6688effe3e393833883a558eea
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-06-23 23:35:38 -0500

    feat: PrimeNG Modules

--- src/app/shared/primeng/primeng.module.ts
+++ src/app/shared/primeng/primeng.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { FieldsetModule } from 'primeng/fieldset';
+import { MenubarModule } from 'primeng/menubar';
+import { PanelModule } from 'primeng/panel';
 import { RippleModule } from 'primeng/ripple';
+import { SplitterModule } from 'primeng/splitter';
+import { TabViewModule } from 'primeng/tabview';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 @NgModule({
   declarations: [],
   imports: [
     RippleModule,
-    ButtonModule
+    ButtonModule,
+    MenubarModule,
+    SplitterModule,
+    PanelModule,
+    FieldsetModule,
+    DividerModule,
+    TabViewModule,
+    ProgressBarModule
   ],
   exports: [
     RippleModule,
-    ButtonModule
+    ButtonModule,
+    MenubarModule,
+    SplitterModule,
+    PanelModule,
+    FieldsetModule,
+    DividerModule,
+    TabViewModule,
+    ProgressBarModule
   ]
 })
 export class PrimengModule { }
