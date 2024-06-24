976388372fa8b8a5e293fcaa9585f6287223386e
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-06-23 23:33:55 -0500

    feat: menu component

---
+++ src/app/shared/components/menu-bar/menu-bar.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrl: './menu-bar.component.css'
+})
+export class MenuBarComponent implements OnInit{
+
+  items: MenuItem[] | undefined;
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+      this.items = [
+        {
+          label: 'Principal',
+          icon: 'pi pi-home',
+          routerLink: '/'
+        },
+        {
+          label: 'Educación',
+          icon: 'pi pi-graduation-cap',
+          routerLink: '/educacion'
+        },
+        {
+          label: 'Experiencia',
+          icon: 'pi pi-briefcase',
+          items: [
+            {
+              label: 'Comware S.A',
+              routerLink: '/experiencia',
+            },
+            {
+              label: 'HIAS',
+              routerLink: '/experiencia',
+            },
+            {
+              label: 'Produbanco',
+              routerLink: '/experiencia',
+            },
+          ]
+        },
+        {
+          label: 'Referencias',
+          icon: 'pi pi-users',
+          routerLink: '/referencias'
+        },
+        {
+          label: 'Certificados',
+          icon: 'pi pi-book',
+          routerLink: '/certificados'
+        },
+        {
+          label: 'Contacto',
+          icon: 'pi pi-phone',
+          routerLink: '/contacto'
+        },
+      ]
+  }
+}
