713b3dca247216011aa95996e551b05b4004e3fc
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-06-24 01:06:23 -0500

    feat: Create Reference Component

---
+++ src/app/pages/reference-page/reference-page.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Reference } from '../../interfaces/reference.interface';
+
+@Component({
+  selector: 'app-reference-page',
+  templateUrl: './reference-page.component.html',
+  styleUrl: './reference-page.component.css'
+})
+export class ReferencePageComponent {
+
+  public references: Reference[] = [
+    {
+      name: 'Óscar Álvarez',
+      company: 'HIAS',
+      position: 'IT Specialist',
+      phone: '+593 99 731 2395',
+      email: 'oscar.alvarez@hias.org'
+    },
+    {
+      name: 'Santiago Andrade',
+      company: 'Comware S.A.',
+      position: 'Ingeniero Especialista',
+      phone: 'sandrade@comware.com.ec',
+      email: '+593 98 704 5930'
+    },
+    {
+      name: 'Christian Albán',
+      company: 'Netdigital S.A.',
+      position: 'Gerente General',
+      phone: '+593 98 469 1464',
+      email: 'calban@netdigital.com.ec'
+    },
+  ]
+}
