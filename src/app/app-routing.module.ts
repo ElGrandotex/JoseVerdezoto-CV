f3240fce1a605f6904f7657a2f69b0daaa5b5e0a
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-06-23 23:35:20 -0500

    feat: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalPageComponent } from './pages/principal-page/principal-page.component';
+import { EducationPageComponent } from './pages/education-page/education-page.component';
+import { ExperiencePageComponent } from './pages/experience-page/experience-page.component';
+import { ReferencePageComponent } from './pages/reference-page/reference-page.component';
+import { CertificatesPageComponent } from './pages/certificates-page/certificates-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PrincipalPageComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'educacion',
+    component: EducationPageComponent
+  },
+  {
+    path: 'experiencia',
+    component: ExperiencePageComponent
+  },
+  {
+    path: 'referencias',
+    component: ReferencePageComponent
+  },
+  {
+    path: 'certificados',
+    component: CertificatesPageComponent
+  },
+  {
+    path: 'contacto',
+    component: ContactPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
