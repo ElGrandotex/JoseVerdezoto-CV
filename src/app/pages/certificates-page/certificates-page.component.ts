cd0c2d0160d33c1f9777423c559a67dc8b96a6de
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-06-26 00:42:58 -0500

    feat: Create certificates page

---
+++ src/app/pages/certificates-page/certificates-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificates-page',
+  templateUrl: './certificates-page.component.html',
+  styleUrl: './certificates-page.component.css'
+})
+export class CertificatesPageComponent {
+
+}
