2c19d10783505b2bdd0b90a4b0ee00994ca7054c
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-07-01 22:07:22 -0500

    fix: Imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimengModule } from './shared/primeng/primeng.module';
-import { MenuBarComponent } from './shared/components/menu-bar/menu-bar.component';
-import { PrincipalPageComponent } from './pages/principal-page/principal-page.component';
-import { EducationPageComponent } from './pages/education-page/education-page.component';
-import { ExperiencePageComponent } from './pages/experience-page/experience-page.component';
-import { ReferencePageComponent } from './pages/reference-page/reference-page.component';
-import { CertificatesPageComponent } from './pages/certificates-page/certificates-page.component';
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
   imports: [
     BrowserModule,
